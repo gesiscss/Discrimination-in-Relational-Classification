946c0629fba152a50d8aa3fa6c47a7d75cd61f1d
Lisette Espin <leespinn@gmail.com>
2019-10-22 14:14:21 +0200

    nbc log likelihood and without

--- org/gesis/relational/network_only_bayes.py
+++ org/gesis/relational/network_only_bayes.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import numpy as np
 from collections import Counter
+from scipy.special import logsumexp
 
 ############################################
 # Class
@@ -58,22 +59,57 @@ class nBC(object):
 
     def predict(self, ego, ni, prior):
         '''
+        https://stats.stackexchange.com/questions/105602/example-of-how-the-log-sum-exp-trick-works-in-naive-bayes
         Predicts the new posterior for node ni given its ego network.
         - ego: ego network centered in ni
         - ni: ego node
         - prior: class prior
         returns ci
         '''
+        return self._predict(ego, ni, prior)
+    
+    def _predict_log(self, ego, ni, prior):
+        
+        if len(list(ego.neighbors(ni))) == 0:
+            return prior.copy()
+            
+        # neighbors counting (conditional probabilities)
+        likelihood = pd.Series([0,0], index=prior.index)
+        for nj in ego[ni]:
+            xj = ego.node[nj][ego.graph['class'] if ego.node[nj]['seed'] else 'xi']
+            likelihood = likelihood.add( np.log(self.condprob.loc[:,xj].copy()) )
+        likelihood = likelihood.subtract( logsumexp(likelihood) )
         
         # class prior
-        ci = prior.copy()
+        prior = np.log( prior.copy() )
         
-        # neighbors counting
+        # posterior
+        ci = prior.add( likelihood )
+        
+        # normalizing
+        ci = ci.subtract( logsumexp(ci) )
+        
+        # eliminating log
+        ci = np.exp( ci )
+
+        return ci
+    
+    def _predict(self, ego, ni, prior):
+        
+        if len(list(ego.neighbors(ni))) == 0:
+            return prior.copy()
+            
+        # neighbors counting (conditional probabilities)
+        ci = pd.Series([1,1], index=prior.index)
         for nj in ego[ni]:
             xj = ego.node[nj][ego.graph['class'] if ego.node[nj]['seed'] else 'xi']
             ci *= self.condprob.loc[:,xj].copy()
-            
-        # normalize
+        ci /= ci.sum()
+        
+        # class prior
+        ci *= prior.copy()
+        
+        # normalizing
         ci /= ci.sum()
         
         return ci
